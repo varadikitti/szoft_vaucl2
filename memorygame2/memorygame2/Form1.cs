3277c4d8cc40b4cbc18565451758bd57b3daba08
Kitti Váradi <kitti.varadi@stud.uni-corvinus.hu>
2024-05-20 00:19:45 +0200

    changes

---
+++ memorygame2/memorygame2/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace memorygame2
+{
+    public partial class Form1 : Form
+    {
+        kartya elozoKartya;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BackgroundImage = Bitmap.FromFile(Properties.Settings.Default.hatterkep);
+            Width = BackgroundImage.Width;
+            Height = BackgroundImage.Height;
+
+            int sorSzam = 0;
+            int[] t = Keveres(16);
+
+
+            for (int s = 0; s < 4; s++)
+            {
+                for (int o = 0; o < 4; o++)
+                {
+                    kartya k = new kartya(s, o, t[sorSzam]);
+                    Controls.Add(k);
+
+                    k.Click += K_Click;
+                    sorSzam++;
+                }
+            }
+        }
+
+        private void K_Click(object sender, EventArgs e)
+        {
+            if (sender is kartya)
+            {
+                kartya k = (kartya)sender;
+
+                if (elozoKartya != null) 
+                {
+                    if (k.kintKepSzam == elozoKartya.kintKepSzam && k != elozoKartya)
+                    {
+                        k.Visible = false;
+                        elozoKartya.Visible = false;
+                    }
+                }
+                elozoKartya = k;
+
+            }
+            
+        }
+
+        int[] Keveres(int kartyaSzam)
+        {
+            int[] tomb = new int[kartyaSzam];
+
+            for (int i = 0; i < kartyaSzam / 2; i++)
+            {
+                tomb[i] = i + 1;
+                tomb[i + kartyaSzam / 2] = i + 1;
+            }
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < kartyaSzam; i++)
+            {
+                int egyik = rnd.Next(kartyaSzam);
+                int masik = rnd.Next(kartyaSzam);
+
+                int koztes = tomb[egyik];
+                tomb[egyik] = tomb[masik];
+                tomb[masik] = koztes;
+            }
+
+            return tomb;
+        }
+    }
+}
---
+++ memorygame2/memorygame2/kartya.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace memorygame2
+{
+    internal class kartya : PictureBox
+    {
+        public int kintKepSzam;
+
+        Timer timer = new Timer();
+
+        public kartya(int sor, int oszlop, int kepSzam)
+        {
+            kintKepSzam = kepSzam;
+
+            Height = Properties.Settings.Default.kepMeret;
+            Width = Properties.Settings.Default.kepMeret;
+
+            Left = oszlop * Properties.Settings.Default.kepTavolsag;
+            Top = sor * Properties.Settings.Default.kepTavolsag;
+
+            Lefordit();
+            //Felfordit();
+
+            this.Click += Kartya_Click;
+            timer.Interval = 2000;
+            timer.Tick += Timer_Tick;
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Lefordit();
+            timer.Stop();
+        }
+
+        private void Kartya_Click(object sender, EventArgs e)
+        {
+            Felfordit();
+            timer.Start();
+        }
+
+        void Felfordit()
+        {
+            Image = Bitmap.FromFile(Properties.Settings.Default.kepKonyvtar + kintKepSzam.ToString() + Properties.Settings.Default.kepUtotag);
+        }
+
+        void Lefordit()
+        {
+            Image = Bitmap.FromFile(Properties.Settings.Default.kepKonyvtar + "card_back" + Properties.Settings.Default.kepUtotag);
+        }
+    }
+}
